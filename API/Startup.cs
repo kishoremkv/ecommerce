b1914047e66b5e1a50851d51ac439b63dbf22079
M K V Kishore <36737425+kishoremkv@users.noreply.github.com>
2023-03-30 03:06:55 +0000

    Added redirects for invalid routes

--- API/Startup.cs
+++ API/Startup.cs
@@ -45,6 +45,7 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStatusCodePagesWithRedirects("/errors/{0}");
             if (env.IsDevelopment())
             {
 
